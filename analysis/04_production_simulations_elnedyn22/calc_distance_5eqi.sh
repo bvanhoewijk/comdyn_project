d888c5a01a9f4dea24262997994f5dfb4cf3e84c
Bas van Hoewijk <basvanhoewijk@gmail.com>
2024-05-03 09:44:53 +0200

    cleanup

--- analysis/04_production_simulations_elnedyn22/calc_distance_5eqi.sh
+++ analysis/04_production_simulations_elnedyn22/calc_distance_5eqi.sh
@@ -12,47 +12,47 @@ do
         echo "File ${tpr_file} does not exist. Continuing with next file."
         continue
     fi
-    # gmx distance \
-    #     -s ${tpr_file} \
-    #     -f ${xtc_file} \
-    #     -oall distances/5eqi/${item}_TM5_TM11_in_rep${i}.com.xvg \
-    #     -n config/5eqi_distance.ndx \
-    #     -select 'com of group TM5_in plus com of group TM11_in'
+    gmx distance \
+        -s ${tpr_file} \
+        -f ${xtc_file} \
+        -oall distances/5eqi/${item}_TM5_TM11_in_rep${i}.com.xvg \
+        -n config/5eqi_distance.ndx \
+        -select 'com of group TM5_in plus com of group TM11_in'
 
-    # gmx distance \
-    #     -s ${tpr_file} \
-    #     -f ${xtc_file} \
-    #     -oall distances/5eqi/${item}_TM5_TM11_out_rep${i}.com.xvg \
-    #     -n config/5eqi_distance.ndx \
-    #     -select 'com of group TM5_out plus com of group TM11_out'
+    gmx distance \
+        -s ${tpr_file} \
+        -f ${xtc_file} \
+        -oall distances/5eqi/${item}_TM5_TM11_out_rep${i}.com.xvg \
+        -n config/5eqi_distance.ndx \
+        -select 'com of group TM5_out plus com of group TM11_out'
 
-    # gmx distance \
-    #     -s ${tpr_file} \
-    #     -f ${xtc_file} \
-    #     -oall distances/5eqi/${item}_TM1_TM7_in_rep${i}.com.xvg \
-    #     -n config/5eqi_distance.ndx \
-    #     -select 'com of group TM1_in plus com of group TM7_in'
+    gmx distance \
+        -s ${tpr_file} \
+        -f ${xtc_file} \
+        -oall distances/5eqi/${item}_TM1_TM7_in_rep${i}.com.xvg \
+        -n config/5eqi_distance.ndx \
+        -select 'com of group TM1_in plus com of group TM7_in'
 
-    # gmx distance \
-    #     -s ${tpr_file} \
-    #     -f ${xtc_file} \
-    #     -oall distances/5eqi/${item}_TM1_TM7_out_rep${i}.com.xvg \
-    #     -n config/5eqi_distance.ndx \
-    #     -select 'com of group TM1_out plus com of group TM7_out'
+    gmx distance \
+        -s ${tpr_file} \
+        -f ${xtc_file} \
+        -oall distances/5eqi/${item}_TM1_TM7_out_rep${i}.com.xvg \
+        -n config/5eqi_distance.ndx \
+        -select 'com of group TM1_out plus com of group TM7_out'
 
-    # gmx distance \
-    #     -s ${tpr_file} \
-    #     -f ${xtc_file} \
-    #     -oall distances/5eqi/${item}_TM2_TM8_in_rep${i}.com.xvg \
-    #     -n config/5eqi_distance.ndx \
-    #     -select 'com of group TM2_in plus com of group TM8_in'
+    gmx distance \
+        -s ${tpr_file} \
+        -f ${xtc_file} \
+        -oall distances/5eqi/${item}_TM2_TM8_in_rep${i}.com.xvg \
+        -n config/5eqi_distance.ndx \
+        -select 'com of group TM2_in plus com of group TM8_in'
 
-    # gmx distance \
-    #     -s ${tpr_file} \
-    #     -f ${xtc_file} \
-    #     -oall distances/5eqi/${item}_TM2_TM8_out_rep${i}.com.xvg \
-    #     -n config/5eqi_distance.ndx \
-    #     -select 'com of group TM2_out plus com of group TM8_out'
+    gmx distance \
+        -s ${tpr_file} \
+        -f ${xtc_file} \
+        -oall distances/5eqi/${item}_TM2_TM8_out_rep${i}.com.xvg \
+        -n config/5eqi_distance.ndx \
+        -select 'com of group TM2_out plus com of group TM8_out'
 
     # gmx distance \
     #     -s ${tpr_file} \
@@ -81,33 +81,33 @@ do
     #     -oall distances/5eqi/${item}_TM5_TM8_in_rep${i}.com.xvg \
     #     -n config/5eqi_distance.ndx \
     #     -select 'com of group TM5_in plus com of group TM8_in'
-    gmx distance \
-        -s ${tpr_file} \
-        -f ${xtc_file} \
-        -oall distances/5eqi/${item}_TM8_TM9_out_rep${i}.com.xvg \
-        -n config/5eqi_distance.ndx \
-        -select 'com of group TM8_out plus com of group TM9_out'
+    # gmx distance \
+    #     -s ${tpr_file} \
+    #     -f ${xtc_file} \
+    #     -oall distances/5eqi/${item}_TM8_TM9_out_rep${i}.com.xvg \
+    #     -n config/5eqi_distance.ndx \
+    #     -select 'com of group TM8_out plus com of group TM9_out'
     
-    gmx distance \
-        -s ${tpr_file} \
-        -f ${xtc_file} \
-        -oall distances/5eqi/${item}_TM8_TM9_in_rep${i}.com.xvg \
-        -n config/5eqi_distance.ndx \
-        -select 'com of group TM8_in plus com of group TM9_in'
+    # gmx distance \
+    #     -s ${tpr_file} \
+    #     -f ${xtc_file} \
+    #     -oall distances/5eqi/${item}_TM8_TM9_in_rep${i}.com.xvg \
+    #     -n config/5eqi_distance.ndx \
+    #     -select 'com of group TM8_in plus com of group TM9_in'
 
-    gmx distance \
-        -s ${tpr_file} \
-        -f ${xtc_file} \
-        -oall distances/5eqi/${item}_TM2_TM3_out_rep${i}.com.xvg \
-        -n config/5eqi_distance.ndx \
-        -select 'com of group TM2_out plus com of group TM3_out'
+    # gmx distance \
+    #     -s ${tpr_file} \
+    #     -f ${xtc_file} \
+    #     -oall distances/5eqi/${item}_TM2_TM3_out_rep${i}.com.xvg \
+    #     -n config/5eqi_distance.ndx \
+    #     -select 'com of group TM2_out plus com of group TM3_out'
     
-    gmx distance \
-        -s ${tpr_file} \
-        -f ${xtc_file} \
-        -oall distances/5eqi/${item}_TM2_TM3_in_rep${i}.com.xvg \
-        -n config/5eqi_distance.ndx \
-        -select 'com of group TM2_in plus com of group TM3_in'
+    # gmx distance \
+    #     -s ${tpr_file} \
+    #     -f ${xtc_file} \
+    #     -oall distances/5eqi/${item}_TM2_TM3_in_rep${i}.com.xvg \
+    #     -n config/5eqi_distance.ndx \
+    #     -select 'com of group TM2_in plus com of group TM3_in'
 
     # gmx distance \
     #     -s ${tpr_file} \
